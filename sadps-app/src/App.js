c76ef0e090689a2a31925e288b37ad52213a6dca
arcvelit <louka.f.s@gmail.com>
2023-11-14 18:37:46 -0500

    Navbar completed and all page components initialized

--- sadps-app/src/App.js
+++ sadps-app/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import DeliveryRequest from "./components/Deliveries/Customer/DeliveryRequest"
+import MakeDeliveryRequest from "./components/Deliveries/Customer/MakeDeliveryRequest"
+import GenerateQuotationProposal from './components/Deliveries/Customer/GenerateQuotationProposal';
+import DefaultHome from './components/Home/DefaultHome';
+import NavigationBar from './components/General/NavigationBar';
+import LeaveAReview from './components/Deliveries/Customer/LeaveAReview';
+import GetTrackingUpdates from './components/Deliveries/Customer/GetTrackingUpdates';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" Component={DeliveryRequest}/>
+      <NavigationBar />
 
+      <Routes>
+        <Route path="/get-tracking-updates" Component={GetTrackingUpdates}/>
+        <Route path="/leave-a-review" Component={LeaveAReview}/>
+        <Route path="/make-delivery-request" Component={MakeDeliveryRequest}/>
+        <Route path="/generate-quotation-proposal" Component={GenerateQuotationProposal}/>
+        <Route path="/" Component={DefaultHome}/>
 
       <Route render={() => <Navigate to="/" />} />
 
