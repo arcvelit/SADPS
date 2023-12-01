50aebf8548cb42b486b83a35714da65e1a4cf8d0
arcvelit <louka.f.s@gmail.com>
2023-12-01 02:34:44 -0500

    Quotation manager

--- sadps-app/src/components/Objects/Controllers/QuotationManager.js
+++ sadps-app/src/components/Objects/Controllers/QuotationManager.js
@@ -4,13 +4,17 @@ export default class QuotationManager extends AbstractManager {
 
     async execute() {
         
+        console.log("Sending quotation.");
         await new Promise((resolve) => {
             setTimeout(() => {
               resolve('Operation completed successfully!');
             }, 2000); // 2000 milliseconds or 2 seconds
           });
 
-          
+        console.log("Quotation received.");
+
+
+
     }
     
 }
