37c433aad67aa41677d468d582bd3a37a2f2d665
arcvelit <louka.f.s@gmail.com>
2023-12-01 00:33:25 -0500

    Payment strategies

--- sadps-app/src/components/Objects/Controllers/PaymentManager.js
+++ sadps-app/src/components/Objects/Controllers/PaymentManager.js
@@ -1,7 +1,17 @@
+import CreditStrategy from '../PaymentStrategies/CreditStrategy'
+
 class PaymentManager extends AbstractManager {
+
+    constructor(strategy = null) {
+        if (strategy === null)
+            this.strategy = new CreditStrategy();
+        this.strategy = strategy;
+    }
+
+    setStrategy(strategy) { this.strategy = strategy; }
 
     execute() {
 
     }
-    
+
 }
---
+++ sadps-app/src/components/Objects/PaymentStrategies/PaymentStrategies.js
@@ -0,0 +1,31 @@
+class PaymentStrategy {
+
+    makeRemoteTransaction() {
+        throw new Error('makeRemoteTransaction must be implemented in payment');
+    }
+
+}
+
+export class PaypalStrategy extends PaymentStrategy {
+
+    async makeRemoteTransaction() {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+    }
+
+}
+
+export class CryptoStrategy extends PaymentStrategy {
+
+    async makeRemoteTransaction() {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+    }
+
+}
+
+export class CreditStrategy extends PaymentStrategy {
+
+    async makeRemoteTransaction() {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+    }
+
+}
